943b068965462b204490bf4e9bfd69e3a67d38f4
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2024-02-23 23:24:22 -0500

    force graph updates

--- src/components/graph/force-citations.tsx
+++ src/components/graph/force-citations.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 
 import { useTheme } from "next-themes"
 
+import * as d3 from "d3-array"
+
 import { ForceGraph2D } from "react-force-graph"
 import { ForceGraphProps, ForceGraphMethods } from "react-force-graph-2d"
 import { ScaleLogarithmic, scaleLog, ScaleLinear, scaleLinear } from "d3-scale"
@@ -91,12 +93,25 @@ export default function CitationGraph({ graphData, originatingPaperId }: { graph
             // const nodeFilterFn = (node: any) => true;
 
             // fgRef.current.zoomToFit(transitionDuration, padding, nodeFilterFn);
-            fgRef.current.d3Force('charge').strength(-75);
+            fgRef.current.d3Force('charge').strength(-100);
         }
     }, [graphData]);
 
-    const valToColor = scaleLog([1, 5, 75, 150], [resolvedTheme === 'dark' ? "gray": "white", "lightblue", "steelblue", "lightgreen"]);
+    const { q1, q2, q3 } = calculateQuartiles(graphData.nodes.map(node => node.val));
+    const maxVal = Math.max(...graphData.nodes.map(node => node.val));
+
+    // const valToColor = scaleLog([1, 5, 75, 150], [resolvedTheme === 'dark' ? "gray": "white", "lightblue", "steelblue", "lightgreen"]);
+
+    const valToColor = scaleLinear([q1, q2, q3, maxVal], [resolvedTheme === 'dark' ? "gray" : "white", "lightblue", "steelblue", "lightgreen"]);
+
     const borderWidthScale = scaleLinear([minCitationCount, maxCitationCount], [0.5, 4]);
+
+    const calculateRadius = (year: number) => {
+        const currentYear = 2024;
+        const yearDiff = currentYear - year;
+        const maxRadius = 12;
+        return Math.max(5, maxRadius - yearDiff * 0.5);
+    };
 
     return (
         <div className="flex flex-col gap-4 items-end">
@@ -105,6 +120,11 @@ export default function CitationGraph({ graphData, originatingPaperId }: { graph
             borderWidthScale={borderWidthScale}
             minCitationCount={minCitationCount}
             maxCitationCount={maxCitationCount}
+            q1={q1}
+            q2={q2}
+            q3={q3}
+            maxVal={maxVal}
+            calculateRadius={calculateRadius}
             />
         <Card ref={containerRef} className="relative w-full h-full min-w-0">
             <ForceGraph2D
@@ -118,10 +138,8 @@ export default function CitationGraph({ graphData, originatingPaperId }: { graph
                 linkColor={() => resolvedTheme === 'dark' ? 'gray' : 'lightgray'}
                 linkWidth={1}
                 nodeCanvasObject={(node, ctx, globalScale) => {
-                    const currentYear = 2024;
-                    const yearDiff = currentYear - (node.val3 as number);
-                    const maxRadius = 10;
-                    const radius = Math.max(5, maxRadius - yearDiff * 0.5);
+                    // Determine the size of the node based on year
+                    const radius = calculateRadius(node.val3 as number);
 
                     // Fill the seed node 
                     const isSeedNode = node.id === originatingPaperId;
@@ -142,7 +160,7 @@ export default function CitationGraph({ graphData, originatingPaperId }: { graph
                     ctx.stroke();
                   }}
                   nodePointerAreaPaint={(node, color, ctx) => {
-                    const radius = Math.max(5, 10 - (2024 - node.val3) * 0.5); // For the interaction area
+                    const radius = Math.max(5, 12 - (2024 - node.val3) * 0.5); // For the interaction area
                     ctx.beginPath();
                     ctx.arc(node.x || 0, node.y || 0, radius, 0, 2 * Math.PI, false);
                     ctx.fillStyle = color;
@@ -153,12 +171,12 @@ export default function CitationGraph({ graphData, originatingPaperId }: { graph
                 <TooltipProvider>
                     <Tooltip>
                         <TooltipTrigger asChild>
-                            <div className="absolute top-4 left-4 p-2 bg-background/90 backdrop-blur-sm rounded-sm">
+                            <div className="absolute top-4 left-4 px-4 py-2 bg-background/90 backdrop-blur-sm rounded-sm border shadow-sm">
                                 <h3 className="text-sm font-medium">{hoverNode.name}</h3>
-                                <p className="text-xs">{hoverNode.val6} ({hoverNode.val3})</p>
-                                <p className="text-xs">Influential citation count: {hoverNode.val}</p>
-                                <p className="text-xs">Citation count: {hoverNode.val2}</p>
-                                <p className="text-xs">Reference count: {hoverNode.val4}</p>
+                                <p className="text-sm">{hoverNode.val6} ({hoverNode.val3})</p>
+                                <p className="text-sm">Influential citation count: {hoverNode.val}</p>
+                                <p className="text-sm">Citation count: {hoverNode.val2}</p>
+                                <p className="text-sm">Reference count: {hoverNode.val4}</p>
                             </div>
                         </TooltipTrigger>
                     </Tooltip>
@@ -170,23 +188,46 @@ export default function CitationGraph({ graphData, originatingPaperId }: { graph
     )
 }
 
+function calculateQuartiles(data: number[]) {
+    const sortedData = data.filter((item): item is number => item !== undefined).sort((a, b) => a - b);
+    const q1 = d3.quantile(sortedData, 0.25);
+    const q2 = d3.quantile(sortedData, 0.5); // Median
+    const q3 = d3.quantile(sortedData, 0.75);
+
+    return {
+        q1: q1 ?? 0, // Consider fallback values or handling for undefined
+        q2: q2 ?? 0,
+        q3: q3 ?? 0
+    };
+}
+
 function GraphLegend ({ 
     colorScale, 
     borderWidthScale,
     minCitationCount,
     maxCitationCount,
+    q1,
+    q2,
+    q3,
+    maxVal,
+    calculateRadius,
 }: { 
-    colorScale: ScaleLogarithmic<string, string, never>, 
+    colorScale: ScaleLinear<string, string, never>, 
     borderWidthScale: ScaleLinear<number, number, never>,
     minCitationCount: number,
     maxCitationCount: number,
+    q1: number,
+    q2: number,
+    q3: number,
+    maxVal: number,
+    calculateRadius: (year: number) => number,
 }) {
-    const colorValues = [1, 5, 75, 150];
+    const colorValues = [q1, q2, q3, maxVal];
     const colorItems = colorValues.map(value => {
         const color = colorScale(value);
         return (
             <div key={value} className="flex items-center mb-2">
-                <div className="w-4 h-4 rounded-full mr-2 border border-black" style={{ backgroundColor: color }}></div>
+                <div className="w-4 h-4 rounded-full mr-2 border-[0.75px] border-black" style={{ backgroundColor: color }}></div>
                 <div className="text-xs">{value}</div>
             </div>
         )
@@ -203,6 +244,20 @@ function GraphLegend ({
         )
     });
 
+    const exampleYears = [2012, 2016, 2020, 2024];
+    const yearItems = exampleYears.map(year => ({
+        year,
+        diameter: calculateRadius(year) * 2,
+    }));
+    const yearCircles = yearItems.map(item => {
+        return (
+            <div key={item.year} className="flex items-center mb-2">
+                <div className="rounded-full mr-2 border-[0.75px] border-black" style={{ width: `${item.diameter}px`, height: `${item.diameter}px` }}></div>
+                <div className="text-xs">{item.year}</div>
+            </div>
+        )
+    });
+
     return (
         <Popover>
             <PopoverTrigger asChild>
@@ -212,7 +267,7 @@ function GraphLegend ({
                 </Button>
             </PopoverTrigger>
             <PopoverContent align="end" className="w-max">
-                <div className="flex items-start">
+                <div className="flex gap-2 items-start">
                     <div className="flex flex-col items-start justify-center mr-4">
                         <h3 className="text-xs font-medium mb-2 w-16">Influential citations</h3>
                         <div className="flex flex-col items-start justify-center">{colorItems}</div>
@@ -221,48 +276,52 @@ function GraphLegend ({
                         <h3 className="text-xs font-medium mb-2 w-16">Total citations</h3>
                         <div className="flex flex-col items-start justify-center">{borderItems}</div>
                     </div>
+                    <div className="flex flex-col items-start justify-center">
+                        <h3 className="text-xs font-medium mb-2 w-16">Citation year</h3>
+                        <div className="flex flex-col items-start justify-center">{yearCircles}</div>
+                    </div>
                 </div>
             </PopoverContent>
         </Popover>
     )
 }
 
-// Shape of the data
-const sampleData = {
-    "nodes": [ 
-        { 
-          "id": "id1",
-          "name": "name1",
-          "val": 1 
-        },
-        { 
-          "id": "id2",
-          "name": "name2",
-          "val": 10 
-        },
-        { 
-          "id": "id3",
-          "name": "name3",
-          "val": 12 
-        },
-        { 
-          "id": "id4",
-          "name": "name4",
-          "val": 24
-        },
-    ],
-    "links": [
-        {
-            "source": "id1",
-            "target": "id2"
-        },
-        {
-            "source": "id2",
-            "target": "id3"
-        },
-        {
-            "source": "id3",
-            "target": "id1"
-        }
-    ]
-}
+// // Shape of the data
+// const sampleData = {
+//     "nodes": [ 
+//         { 
+//           "id": "id1",
+//           "name": "name1",
+//           "val": 1 
+//         },
+//         { 
+//           "id": "id2",
+//           "name": "name2",
+//           "val": 10 
+//         },
+//         { 
+//           "id": "id3",
+//           "name": "name3",
+//           "val": 12 
+//         },
+//         { 
+//           "id": "id4",
+//           "name": "name4",
+//           "val": 24
+//         },
+//     ],
+//     "links": [
+//         {
+//             "source": "id1",
+//             "target": "id2"
+//         },
+//         {
+//             "source": "id2",
+//             "target": "id3"
+//         },
+//         {
+//             "source": "id3",
+//             "target": "id1"
+//         }
+//     ]
+// }
