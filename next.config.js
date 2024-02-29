e1347d8b6b74fdcb323479cfc39455f5e362daa6
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2024-02-29 12:26:56 -0500

    canvas.node config fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const withMDX = require('@next/mdx')()
 const nextConfig = {
     // Include mdx files
     pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
-}
+    webpack: (config, { buiildId, dev, isServer, defaultLoaders, webpack }) => {
+        config.resolve.alias.canvas = false
+        config.resolve.alias.encoding = false
+        return config;
+    },
+};
 
 module.exports = withMDX(nextConfig)
