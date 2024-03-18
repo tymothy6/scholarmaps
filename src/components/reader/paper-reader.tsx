2c63aed9c7a3e29747a873a7155eca93ce60dc23
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2024-03-18 11:42:21 -0400

    remove pdfjs from paper-reader

--- src/components/reader/paper-reader.tsx
+++ src/components/reader/paper-reader.tsx
@@ -2,16 +2,16 @@
 
 import * as React from 'react';
 
-import { pdfjs, Document, Page } from 'react-pdf';
+// import { pdfjs, Document, Page } from 'react-pdf';
 
 export function PaperReader({ source }: { source: string }) {
-    pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`;
+    // pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js`;
 
-    const [numPages, setNumPages] = React.useState<number | null>(null);
+    // const [numPages, setNumPages] = React.useState<number | null>(null);
 
-    function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
-        setNumPages(numPages);
-    }
+    // function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
+    //     setNumPages(numPages);
+    // }
 
     return (
         <embed
