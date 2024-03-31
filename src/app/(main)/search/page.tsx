58cf33b997659001c9b87a4c52d4bb86235c49c8
Tim Ng <108041576+tymothy6@users.noreply.github.com>
2024-03-31 13:40:08 -0400

    add error handling to search page

--- src/app/(main)/search/page.tsx
+++ src/app/(main)/search/page.tsx
@@ -4,7 +4,12 @@ import { SearchPaperResult, columns } from './tables/search-columns'
 import { SearchResultTable } from './tables/search-table'
 import { SearchTableSkeleton } from './search-skeleton'
 import { RecentSearches } from './recents'
-import { StringValidation } from 'zod'
+import { 
+    Alert,
+    AlertDescription,
+    AlertTitle,
+ } from '@/components/ui/alert'
+import { AlertCircle } from 'lucide-react'
 
 export async function generateMetadata({ searchParams }: { searchParams: {[key: string]: string | undefined } }) {
     const title = searchParams['query'] ? `Search results for ${searchParams['query']}` : 'Search';
@@ -12,6 +17,7 @@ export async function generateMetadata({ searchParams }: { searchParams: {[key: 
     return { title, description };
 }
 
+// Shape of the response from a successful call
 interface SearchResponse { 
     total: number; 
     offset: number; 
@@ -21,14 +27,14 @@ interface SearchResponse {
     updatedAt: Date | null;
  }
 
-// Shape of the response from a failed API call
+// Shape of the response from a failed call
 interface ErrorResponse {
     message: string;
     error: JSON;
     createdAt: Date | null;
  }
 
-async function getSearchResults(searchQuery: string): Promise<SearchResponse> {
+async function getSearchResults(searchQuery: string): Promise<SearchResponse | ErrorResponse> {
     const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
     const queryParams = new URLSearchParams({ query: searchQuery });
     const url = `${baseUrl}/api/search?${queryParams.toString()}`;
@@ -37,15 +43,16 @@ async function getSearchResults(searchQuery: string): Promise<SearchResponse> {
         const response = await fetch(url);
 
         if (!response.ok) {
-            throw new Error(`Relevance API responded with status code ${response.status}: ${response.statusText}`);
+            const errorData: ErrorResponse = await response.json();
+            console.error('Error fetching search results:', errorData);
+            return errorData;
         }
 
-        const data = await response.json();
-        // console.log ('getSearchResults response:', data);
+        const data: SearchResponse = await response.json();
         return data;
     } catch (error) {
         console.error('Error fetching search results:', error);
-        return { total: 0, offset: 0, next: 0, data: [], createdAt: null, updatedAt: new Date()};
+        return { total: 0, offset: 0, next: 0, data: [], createdAt: null, updatedAt: new Date(), message: 'Failed to fetch search results'};
     }
 }
 
@@ -95,17 +102,33 @@ export default async function Search( { searchParams }: SearchProps ) {
     async function SearchResultsCard() {
         let results: SearchPaperResult[] = [];
         let timestamp: Date | null = new Date();
+        let errorMessage: string | null = null;
 
         if (searchQuery) {
             const response = await getSearchResults(searchQuery);
-            results = response.data;
-            timestamp = response.createdAt;
+
+            if ('data' in response) { // successful response
+                results = response.data;
+                timestamp = response.createdAt;
+            } else {
+                errorMessage = response.message; // error response
+            }
         }
 
         return (
             <div className="flex flex-col gap-2">
-                {timestamp && <p className="text-sm font-medium text-muted-foreground">Last updated: {timestamp.toLocaleString()}</p>}
-                <SearchResultTable columns={columns} data={results} />
+                {errorMessage ? (
+                    <Alert variant="destructive">
+                        <AlertCircle className="w-4 h-4" />
+                        <AlertTitle>Error</AlertTitle>
+                        <AlertDescription>{errorMessage}</AlertDescription>
+                    </Alert>
+                ) : (
+                    <>
+                        {timestamp && <p className="mt-2 text-sm font-medium text-muted-foreground">Last updated: {timestamp.toLocaleString()}</p>}
+                        <SearchResultTable columns={columns} data={results} />
+                    </>
+                )}
             </div>
         )
     }
@@ -114,7 +137,7 @@ export default async function Search( { searchParams }: SearchProps ) {
         <section className="bg-background p-4 absolute top-10 lg:left-[16.666%] lg:p-8 flex flex-col gap-2 w-full overflow-x-hidden lg:w-5/6">
             <h1 className="mt-2 lg:mt-0 text-xl lg:text-2xl font-semibold lg:font-bold">Search {searchQuery ? <span>for<span className="ml-2 px-2 py-1 text-lg lg:text-xl border bg-secondary rounded font-mono">{searchQuery}</span></span> : ''}</h1>
             <div className="w-full">
-                { searchQuery ?
+                {searchQuery ?
                     <Suspense fallback={<SearchTableSkeleton />}>
                     <SearchResultsCard />
                     </Suspense>
